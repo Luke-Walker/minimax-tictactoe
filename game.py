f29a19ca9650d52dd5226a858fc8bd11df886587
Luke Walker <lukeryanwalker2000@gmail.com>
2024-04-08 21:30:40 -0700

    Update game.py
    - Cleaned-up some redundant code
    - Added leading underscore to functions

--- game.py
+++ game.py
@@ -1,19 +1,19 @@
 from typing import List, Optional, Tuple
 
 # Returns the letter of the non-specified player
-def switch_player(player: str) -> str:
+def _switch_player(player: str) -> str:
     return 'X' if player == 'O' else 'O'
 
 # Returns a copy of the given board
-def copy_board(board: List[List]) -> List[List]:
+def _copy_board(board: List[List]) -> List[List]:
     return [[x for x in row] for row in board]
 
 # Returns whether the specified spot in the board is empty or not
-def empty_index(board: List[List], row: int, col: int) -> bool:
+def _empty_index(board: List[List], row: int, col: int) -> bool:
     return board[row][col] == ' '
 
 # Returns the winner's letter, or None if there is no winner
-def check_winner(board: List[List]) -> Optional[str]:
+def _check_winner(board: List[List]) -> Optional[str]:
     for letter in ['X', 'O']:
         # Check rows
         for i in range(3):
@@ -64,7 +64,7 @@ def check_winner(board: List[List]) -> Optional[str]:
     return None
 
 # Returns whether the board is full or not
-def check_draw(board: List[List]) -> bool:
+def _check_draw(board: List[List]) -> bool:
     for i in range(3):
         for j in range(3):
             if board[i][j] == ' ':
@@ -73,7 +73,7 @@ def check_draw(board: List[List]) -> bool:
     return True
 
 # Prints the game board
-def print_board(board: List[List]):
+def _print_board(board: List[List]):
     for i in range(3):
         for j in range(3):
             print(board[i][j], end=("\n" if j == 2 else " | "))
@@ -85,7 +85,7 @@ class Game:
     def __init__(self, human: str):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
         self.human = human
-        self.cpu = switch_player(human)
+        self.cpu = _switch_player(human)
         self.turn = 'X'
 
     # Game loop
@@ -94,23 +94,25 @@ class Game:
         while True:
             self._take_turn()
 
-            winner = check_winner(self.board)
+            winner = _check_winner(self.board)
 
             if winner:
-                print_board(self.board)
+                _print_board(self.board)
                 return winner
             
-            if check_draw(self.board):
+            if _check_draw(self.board):
                 return None
 
     # Prompts the appropriate player to take their turn and updates the board accordingly
     def _take_turn(self):
-        print_board(self.board)
+        _print_board(self.board)
+
+        row, col = 0, 0
 
         if self.turn == self.human:
-            input_num, row, col = 0, 0, 0
+            input_num = 0
             
-            while input_num < 11 or input_num > 33 or not empty_index(self.board, row, col):
+            while input_num < 11 or input_num > 33 or not _empty_index(self.board, row, col):
                 user_input = input("Your turn (<row><col>, 11-33): ")
 
                 if not user_input.isdigit():
@@ -119,18 +121,14 @@ class Game:
                 input_num = int(user_input)
                 row = (input_num // 10) - 1
                 col = (input_num % 10) - 1
-
-            self.board[row][col] = self.human
-
-            self.turn = self.cpu 
         else:
             print("CPU's turn.")
 
             row, col = self._minimax()
-            self.board[row][col] = self.cpu
 
-            self.turn = self.human
-        
+        self.board[row][col] = self.turn
+        self.turn = _switch_player(self.turn)
+
         print("\n=================================\n")
 
     # Returns the optimal move (row, col)
@@ -139,10 +137,10 @@ class Game:
 
         for i in range(3):
             for j in range(3):
-                if not empty_index(self.board, i, j):
+                if not _empty_index(self.board, i, j):
                     continue
 
-                new_board = copy_board(self.board)
+                new_board = _copy_board(self.board)
                 new_board[i][j] = self.cpu
 
                 value = self._dfs(new_board, self.human)
@@ -154,12 +152,12 @@ class Game:
 
     # Returns the minimax value of the provided board, starting on 'player's turn
     def _dfs(self, board: List[List], player: str) -> int:
-        winner = check_winner(board)
+        winner = _check_winner(board)
 
         if winner:
             return 1 if winner == self.cpu else -1
         
-        if check_draw(board):
+        if _check_draw(board):
             return 0
         
         # CPU player's turn
@@ -168,13 +166,13 @@ class Game:
 
             for i in range(3):
                 for j in range(3):
-                    if not empty_index(board, i, j):
+                    if not _empty_index(board, i, j):
                         continue
 
-                    new_board = copy_board(board)
+                    new_board = _copy_board(board)
                     new_board[i][j] = self.cpu
                     
-                    max_value = max(max_value, self._dfs(new_board, switch_player(self.cpu)))
+                    max_value = max(max_value, self._dfs(new_board, _switch_player(self.cpu)))
             
             return max_value
         # Human player's turn
@@ -183,12 +181,12 @@ class Game:
 
             for i in range(3):
                 for j in range(3):
-                    if not empty_index(board, i, j):
+                    if not _empty_index(board, i, j):
                         continue
 
-                    new_board = copy_board(board)
+                    new_board = _copy_board(board)
                     new_board[i][j] = self.human
 
-                    min_value = min(min_value, self._dfs(new_board, switch_player(self.human)))
+                    min_value = min(min_value, self._dfs(new_board, _switch_player(self.human)))
 
             return min_value
